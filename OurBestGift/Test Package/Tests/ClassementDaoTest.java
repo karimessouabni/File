16cb9b43f141d91fa52887a500f744af7ab204d2
karim <karimessouabni0@gmail.com>
2013-05-21 18:52:52 +0200

    Signed-off-by: karim <karimessouabni0@gmail.com>

---
+++ OurBestGift/Test Package/Tests/ClassementDaoTest.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Administration_des_Fournisseurs.Classement;
+import DAO.AbstractDAOFactory;
+import DAO.DAO;
+import DAO.FactoryType;
+
+public class ClassementDaoTest {
+
+	
+	 @Test
+	   public void testFindById() {
+		 DAO<Classement> fDAO = AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY).getClassementDAO();
+		 	Classement u = fDAO.find(3);
+	       assertEquals(3, u.getId());//assertEquals qui consiste à comparer une valeur attendue (1er argument), à un résultat (2ème argument)
+	       //Si le résultat est égal à la valeur attendue, alors l'exécution se poursuit, sinon une exception est levée et la méthode de test est interrompue
+
+	       assertEquals("Loisir", u.getNom());
+	       assertEquals("musique", u.getSousClassement());
+	    
+	   }
+
+	@Test
+
+	   public void testCreate() throws Exception {
+		String myLocation = "Paris";
+		
+		Classement c = new Classement(7, "loisir", "musique" ); 
+		DAO<Classement> fDAO = AbstractDAOFactory.getFactory(FactoryType.DAO_FACTORY).getClassementDAO();
+		fDAO.create(c);
+	    System.out.println("Termine");
+	
+	}
+}
